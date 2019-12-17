87111122e0462e951a3a851fcd2ecc6c950d8dd8
alliS960 <asmith24@nscds.org>
2019-12-17 19:08:26 +0000

    started emoji thing

---
+++ emojicoloring/coloring.js
@@ -0,0 +1,11 @@
+$(".pupilImage").click(
+    function(){
+        alert("i wish it was friday");
+    }
+);
+
+$(".mouthImage").click(
+    function(){
+       var clickedMouth = $(this).css("background-image");
+    }
+);
