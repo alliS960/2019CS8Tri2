33b966eb21717ea24d260913d76b58c0e7f4c199
alliS960 <asmith24@nscds.org>
2020-01-08 20:58:04 +0000

    tried stars

---
+++ nightscene/star.js
@@ -0,0 +1,5 @@
+for(var c=0; c<10; c++){ 
+       var x = Math.floor((Math.random() * 1000) + 1);
+       var y = Math.floor((Math.random() * 1000) +1);
+        $('body').append("<div class='star'style='top:40px left; 100px;'></div>");
+        }
