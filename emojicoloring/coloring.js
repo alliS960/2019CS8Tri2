02522b0882bb3a59e41dc73673c6daa2428efb46
alliS960 <asmith24@nscds.org>
2020-02-03 19:05:48 +0000

    made things clickable

--- emojicoloring/coloring.js
+++ emojicoloring/coloring.js
@@ -1,11 +1,20 @@
 $(".pupilImage").click(
     function(){
-        alert("i wish it was friday");
+        var clickedPupil = $(this).css("background-image")
+        $(".pupil").css("background-image",clickedPupil);
     }
 );
 
 $(".mouthImage").click(
     function(){
-       var clickedMouth = $(this).css("background-image");
+       var clickedMouth = $(this).css("background-image")
+       $("#mouth").css("background-image",clickedMouth);
+    }
+);
+
+$(".color").click(
+    function(){
+        var color = $(this).css("background-color")
+        $("#face").css("background-color", color)
     }
 );
